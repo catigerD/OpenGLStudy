70e26ba0162f67c02385faab980751effe7ae65d
dengchong.999 <dengchong.999@bytedance.com>
2019-06-04 11:41:16 +0800

    opengl 着色器---更多属性:exerciese1:修改顶点着色器，让三角形上下颠倒

--- shader/3.3.shader-0103.vs.glsl
+++ shader/3.3.shader-0103.vs.glsl
@@ -5,6 +5,7 @@ layout(location = 1) in vec3 aColor;
 out vec3 ourColor;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    //    gl_Position = vec4(aPos, 1.0);
+    gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);//exerciese1:修改顶点着色器，让三角形上下颠倒
     ourColor = aColor;
 }
